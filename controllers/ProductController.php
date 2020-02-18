56b2d13b27081fe62a74faf534fb0e68a969662c
lapuce1988 <49413546+lapuce1988@users.noreply.github.com>
2020-02-18 17:08:50 +0100

    update controllers

---
+++ controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+class ProductController extends BaseController{
+    protected $name = 'Product';
+    protected $id;
+
+    public function __construct($id)
+    {
+        $this->smarty = new SmartyLoader();
+        $this->id = $id;
+        $this->renderView();
+    }
+
+    protected function getTemplateVars()
+    {
+        return array(
+            "controller" => $this->name,
+            "product" => new Product($this->id)
+        );
+    }
+
+    public static function getProducts(){
+        $pdo = DB::getInstance();
+        $requete = 'SELECT * FROM product';
+
+        $stmt = $pdo->prepare($requete);
+        $stmt->execute();
+
+        $rows = $stmt->fetchAll();
+        $products = [];
+
+        foreach ($rows as $key => $row){
+            $products[] = Product::fromRow(new Product(), $row);
+        }
+        return $products;
+    }
+
+}
